19586fed8d3c6a164e66e9b9f450bc1de9c48ab6
Jason R. Coombs <jaraco@jaraco.com>
2014-09-24 12:46:32 -0400

    Adding test for RevivedDistribution

---
+++ test_cheese.py
@@ -0,0 +1,9 @@
+from six.moves import urllib
+
+from jaraco.packaging import cheese
+
+def test_revived_distribution():
+	base = 'https://pypi.python.org/packages/source/'
+	nose_timer_path = 'n/nose-timer/nose-timer-0.3.0.tar.gz'
+	url = urllib.parse.urljoin(base, nose_timer_path)
+	cheese.RevivedDistribution(url)
