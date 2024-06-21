c19048821be837865e7c494be1562c80afbe817d
Jason R. Coombs <jaraco@jaraco.com>
2024-06-21 12:18:01 -0400

    Fixed issue in rendering non-unique keys in print-metadata.
    Added a covering test.

--- jaraco/packaging/print-metadata.py
+++ jaraco/packaging/print-metadata.py
@@ -2,10 +2,14 @@ from . import metadata
 
 
 def main():
+    """
+    >>> main()
+    Metadata-Version: ...
+    """
     md = metadata.load('.')
     # cannot print(md) due to python/cpython#119650
-    for key in md:
-        print(key, md[key], sep=': ')
+    for key, value in md.items():
+        print(key, value, sep=': ')
 
 
 __name__ == '__main__' and main()
