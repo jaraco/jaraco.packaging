6af5a38516e84b277cd979631d1f70e89fb22fcd
Avasam <samuel.06@hotmail.com>
2025-02-05 23:58:08 -0500

    Strict type checking and py.typed

--- jaraco/packaging/print-metadata.py
+++ jaraco/packaging/print-metadata.py
@@ -1,15 +1,22 @@
+from typing import TYPE_CHECKING, cast
+
 from . import metadata
 
+if TYPE_CHECKING:
+    from importlib_metadata._adapters import Message
 
-def main():
+
+def main() -> None:
     """
     >>> main()
     Metadata-Version: ...
     """
-    md = metadata.load('.')
+    # cast: Default PathDistribution.metadata is a Message
+    md = cast("Message", metadata.load('.'))
     # cannot print(md) due to python/cpython#119650
     for key, value in md.items():
         print(key, value, sep=': ')
 
 
-__name__ == '__main__' and main()
+if __name__ == '__main__':
+    main()
